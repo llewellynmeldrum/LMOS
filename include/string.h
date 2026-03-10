f64de5185c451fbea60dff61036c3ed32173f322
llewellynmeldrum <llewellyn.meldrum1@students.mq.edu.au>
2026-03-10 17:12:45 +1100

    Moved the files being carried over from V01 to their correct positions.
    Also deleted the rest of the things i wont be carrying over

--- include/string.h
+++
@@ -1,3 +0,0 @@
-#pragma once
-#include <stddef.h>
-size_t strlen(const char *str);
--- verifyKernelMutliboot.zsh
+++
@@ -1,5 +0,0 @@
-if grub-file --is-x86-multiboot bin/lmos; then
-    echo "Multiboot confirmed (grub-file) ret 0"
-else
-    echo "Error: the file is not multiboot compatible."
-fi
